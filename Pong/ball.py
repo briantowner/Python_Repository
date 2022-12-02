fb8a6a3f0801185847dfd647f38d63c71c13fca0
briantowner <116382436+briantowner@users.noreply.github.com>
2022-12-01 21:54:38 -0500

    pong
    pong

---
+++ Pong/ball.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+import random
+
+# STARTING_ANGLE = random.randint(0, 360)
+STARTING_ANGLE = 45
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.setheading(STARTING_ANGLE)
+
+    def move(self):
+        self.forward(20)
+
+    def rebound(self):
+        current_heading = self.heading()
+        new_heading = -current_heading
+        self.setheading(new_heading)
+
+    def paddle(self):
+        current_heading = self.heading()
+        new_heading = current_heading+(180-90-current_heading)*2
+        self.setheading(new_heading)
+
